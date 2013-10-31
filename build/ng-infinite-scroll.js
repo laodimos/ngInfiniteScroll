9970d00b215196de8c8fca522254317117957f52
laodimos <laodimos@gmail.com>
2013-10-31 18:31:00 +0200

    fixed window height problem

--- build/ng-infinite-scroll.js
+++ build/ng-infinite-scroll.js
@@ -28,10 +28,10 @@ mod.directive('infiniteScroll', [
         }
         handler = function() {
           var elementBottom, remaining, shouldScroll, windowBottom;
-          windowBottom = $window.height() + $window.scrollTop();
+          windowBottom = $window.innerHeight() + $window.scrollTop();
           elementBottom = elem.offset().top + elem.height();
           remaining = elementBottom - windowBottom;
-          shouldScroll = remaining <= $window.height() * scrollDistance;
+          shouldScroll = remaining <= $window.innerHeight() * scrollDistance;
           if (shouldScroll && scrollEnabled) {
             if ($rootScope.$$phase) {
               return scope.$eval(attrs.infiniteScroll);
